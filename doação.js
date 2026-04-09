35d017ed00f2ecdad6a46d529955cf0daf780294
enzo_vegini <enzo_vegini@estudante.sesisenai.org.br>
2026-04-09 08:36:45 -0300

    add-html quasse terminando

---
+++ doação.js
@@ -0,0 +1,12 @@
+  document.getElementById("formAdocao").addEventListener("submit", function (e) {
+  
+  let nome = document.getElementById("nome").value;
+  let email = document.getElementById("email").value;
+  let telefone = document.getElementById("telefone").value;
+  let sangue = document.getElementById("sangue").value;
+  let idade = document.getElementById("idade").value;
+  let cidade = document.getElementById("cidade").value;
+  let estado = document.getElementById("estado").value;
+    let peso = document.getElementById("peso").value;
+
+  });
