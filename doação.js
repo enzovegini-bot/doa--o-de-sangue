c0ed6c67c8e4dead35904e1c43b1844a69815528
EnzoVegini <enzo_vegini@estudante.sesisenai.org.br>
2026-05-11 18:46:33 -0300

    add-metade js

--- doação.js
+++ doação.js
@@ -1,12 +1,33 @@
-  document.getElementById("formAdocao").addEventListener("submit", function (e) {
-  
-  let nome = document.getElementById("nome").value;
-  let email = document.getElementById("email").value;
-  let telefone = document.getElementById("telefone").value;
-  let sangue = document.getElementById("sangue").value;
-  let idade = document.getElementById("idade").value;
-  let cidade = document.getElementById("cidade").value;
-  let estado = document.getElementById("estado").value;
-    let peso = document.getElementById("peso").value;
+ let doadores = [];
 
-  });
+document.getElementById("FormularioDoacao").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+
+    let nome = document.getElementById("nome").value.trim();
+    let email = document.getElementById("email").value.trim();
+    let idade = parseFloat(document.getElementById("idade").value);
+    let peso = parseFloat(document.getElementById("peso").value);
+    let tipo_sangue = document.getElementById("tipo_sangue").value;
+    let telefone = document.getElementById("telefone").value.trim();
+    let cidade = document.getElementById("cidade").value.trim();
+    let estado = document.getElementById("estado").value.trim();
+
+
+
+    if (!nome.includes(" ")) {
+        alert("Digite nome e sobrenome");
+        return;
+    }
+        if (!email.includes("@")) return alert(" Email inválido");
+        if (isNaN(idade) || idade < 16) return alert("Obrigatório ter 16 anos ou mais");
+        if (isNaN(peso) || peso < 49) return alert("Peso inválido");
+        if (telefone.length < 8 || isNaN(telefone)) return alert("Telefone inválido");
+        if (isNaN(idade) || idade < 0) return alert("Idade inválida");
+          
+      if (cidade === "" || estado === "") {
+            alert("Preencha todos os campos");
+            return;
+        }
+
+      });
